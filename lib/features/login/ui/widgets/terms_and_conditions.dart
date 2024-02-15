7c332b0bc615dc465fcae6d9a5b9aaf76eef6d18
ahmed taha <bjabotaha@yahoo.com>
2024-02-15 05:02:42 +0200

    login added

---
+++ lib/features/login/ui/widgets/terms_and_conditions.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/theming/styles.dart';
+
+class TermsAndConditions extends StatelessWidget {
+  const TermsAndConditions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+        children: [
+          TextSpan(
+            text: 'By logging, you agree to our',
+            style: TextStyles.font13GrayRegular.copyWith(fontSize: 14.sp),
+          ),
+          TextSpan(
+            text: ' Terms & Conditions',
+            style: TextStyles.font14DarkBlueMedium,
+          ),
+          TextSpan(
+            text: ' and',
+            style: TextStyles.font13GrayRegular.copyWith(fontSize: 14.sp),
+          ),
+          TextSpan(
+            text: ' Privacy Policy',
+            style: TextStyles.font14DarkBlueMedium.copyWith(height: 1.5.h),
+          ),
+        ]
+      ),
+    );
+  }
+}
